6f3878b7dc5762d1da410b3458295ab619849e92
Samarina Sarwar Sachi <samarinasachi12@gmail.com>
2025-12-30 11:38:44 +0600

    Updated main.s

--- main.s
+++ main.s
@@ -4,14 +4,32 @@
     EXPORT main
 
     EXTERN InitEmployees
-	EXTERN BonusCalc
-    
+    EXTERN LoadAttendance
+    EXTERN LeaveManagement
+    EXTERN OvertimeCalc
+    EXTERN AllowanceCalc
+    EXTERN TaxCalc
+    EXTERN NetSalaryCalc
+    EXTERN DeptSummary
+    EXTERN BonusCalc
+	EXTERN SortEmployees
+    EXTERN UART_PaySlip
+	EXTERN Enable_ITM
 
 main
 	PUSH    {R4-R7, LR}
 	BL      InitEmployees
+	BL      LoadAttendance
+	BL      LeaveManagement
+	BL      OvertimeCalc
+	BL      AllowanceCalc
+	BL      TaxCalc
+	BL      NetSalaryCalc
+	BL      DeptSummary
 	BL      BonusCalc
-	
+	BL      SortEmployees
+	BL      Enable_ITM
+	BL      UART_PaySlip
 	POP     {R4-R7, LR}
 loop_end
 	B       loop_end
